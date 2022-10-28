d21a789b0c6a76f439f75e51fce689c1910825a1
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-10-28 19:32:41 -0400

    Added a class to represent a neural network

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use neural_net::{WeightLayer, DataLayer};
+
+struct NeuralNetwork {
+    layers: Vec<WeightLayer>,
+}
+
+impl NeuralNetwork {
+    fn new() -> NeuralNetwork {
+        NeuralNetwork { layers: Vec::new() }
+    }
+
+    fn from_csv_file(csv_file_path: &str) -> NeuralNetwork {
+        // This is placeholder, at least for now.
+        NeuralNetwork::new()
+    }
+
+
+    /// Function that basically just runs the neural network and returns the output. This is rather
+    /// simple as well.
+    fn run(&self, input_layer: &DataLayer) -> DataLayer {
+        let mut current_data_layer = input_layer.clone();
+        self.layers.iter().for_each(|weight_layer| {
+            current_data_layer = weight_layer.pass_data_through(&current_data_layer);
+        });
+
+        current_data_layer
+    }
+
+    fn add_layer(&mut self, layer: &WeightLayer) {
+        self.layers.push(layer.clone());
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
