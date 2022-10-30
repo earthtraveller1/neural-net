a47f8256d8c4f16f91cbc9ec36f69489bab71301
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-10-30 14:32:01 -0400

    Added a way to deserialize a WeightLayer from CSV files.

--- src/lib.rs
+++ src/lib.rs
@@ -58,6 +58,41 @@ impl WeightLayer {
             output_count,
         }
     }
+    
+    // Note that this method is written under the foolish assumption that the  
+    // CSV file that was entered into this function is not irregular. In other 
+    // words, the CSV file must be (for a lack of a better word) "rectangular" 
+    // or weird things might happen (most likely a panic or corrupted layouts) XXXDD.
+    pub fn from_csv_file(file_path: &str) -> Result<WeightLayer, std::io::Error> {
+        let mut elements = Vec::new();
+        let mut input_count = 0;
+        let mut output_count = 0;
+        
+        let file_contents = fs::read_to_string(file_path)?;
+        
+        file_contents.split("\n").for_each(|line| {
+            if input_count == 0 {
+                let mut value_count = 0 as usize;
+                
+                line.split(",").for_each(|value| {
+                    value_count += 1;
+                    elements.push(value.parse::<f32>().unwrap_or_else(|_| 0.0));
+                });
+                
+                input_count = value_count;
+            } else {
+                line.split(",").for_each(|value| {
+                    elements.push(value.parse::<f32>().unwrap_or_else(|_| 0.0));
+                });
+            }
+            
+            output_count += 1;
+        });
+        
+        Ok(WeightLayer {
+            elements, input_count, output_count
+        })
+    }
 
     pub fn pass_data_through(&self, input_data: &DataLayer) -> DataLayer {
         let mut output = Vec::with_capacity(self.output_count);
