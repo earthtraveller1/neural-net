69a5b9af15053c0c5cb7f396dc2b703e2278a8e2
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-10-30 14:41:08 -0400

    Added a unit test.

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,5 @@
 use super::DataLayer;
+use super::WeightLayer;
 
 #[test]
 fn data_layer_csv_deserialization() {
@@ -8,4 +9,23 @@ fn data_layer_csv_deserialization() {
     assert_eq!(test_data[2], 3.0);
     assert_eq!(test_data[3], 4.0);
     assert_eq!(test_data[4], 5.0);
+}
+
+#[test]
+fn weight_layer_csv_and_get_methods() {
+    let test_data = WeightLayer::from_csv_file("data/testweights.csv").expect("Failed to load data/testweights.csv");
+    assert_eq!(test_data.get_value(0, 0), 1.1);
+    assert_eq!(test_data.get_value(1, 0), 1.2);
+    assert_eq!(test_data.get_value(2, 0), 3.0);
+    assert_eq!(test_data.get_value(3, 0), 4.4);
+    
+    assert_eq!(test_data.get_value(0, 1), 3.1);
+    assert_eq!(test_data.get_value(1, 1), 1.1);
+    assert_eq!(test_data.get_value(2, 1), 2.3);
+    assert_eq!(test_data.get_value(3, 1), 1.2);
+    
+    assert_eq!(test_data.get_value(0, 2), 4.5);
+    assert_eq!(test_data.get_value(1, 2), 2.2);
+    assert_eq!(test_data.get_value(2, 2), 9.8);
+    assert_eq!(test_data.get_value(3, 2), 8.8);
 }
