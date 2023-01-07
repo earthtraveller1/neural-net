479cb8dbf2d69f3ac3b84d4890a5c3a1f817800f
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-01-07 15:09:31 -0500

    Added a basic Matrix class.

--- src/math.rs
+++ src/math.rs
@@ -9,10 +9,12 @@ struct Vector {
 }
 
 impl Vector {
+    /// Creates a new Vector with the specified data.
     fn from_vec(data: &Vec<f64>) -> Vector {
         Vector { data: data.clone() }
     }
 
+    /// Creates a new Vector and fills it with random data.
     fn from_random(len: usize) -> Vector {
         let mut data = Vec::with_capacity(len);
 
@@ -35,5 +37,46 @@ impl Add for Vector {
         }
 
         Vector { data }
+    }
+}
+
+struct Matrix {
+    data: Vec<f64>,
+    rows: usize,
+    cols: usize,
+}
+
+impl Matrix {
+    /// Creates a new Matrix and fills it with random data.
+    fn new(rows: usize, cols: usize) -> Matrix {
+        let mut data = Vec::with_capacity(rows * cols);
+        for _ in 0..data.len() {
+            data.push(rand::random());
+        }
+
+        Matrix { data, rows, cols }
+    }
+
+    /// Creates a new Matrix from a 2D array. The 2D slice must not have an
+    /// irregular shape, or else it will be a runtime error.
+    fn from_2d_slice(slice: &[&[f64]]) -> Matrix {
+        let rows = slice.len();
+        let cols = slice[0].len();
+
+        let mut data = Vec::with_capacity(rows * cols);
+
+        slice
+            .iter()
+            .for_each(|row| row.iter().for_each(|x| data.push(*x)));
+
+        Matrix { data, rows, cols }
+    }
+
+    fn get(&self, row: usize, col: usize) -> f64 {
+        self.data[col + row * self.rows]
+    }
+
+    fn get_mut(&mut self, row: usize, col: usize) -> &mut f64 {
+        &mut (self.data[col + row * self.rows])
     }
 }
